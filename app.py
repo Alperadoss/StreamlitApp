2971d8b95a3f104530942351e4507e85ade6cecb
aktasbatuhan <101873025+aktasbatuhan@users.noreply.github.com>
2024-06-06 14:56:48 +0300

    added llm analysis.py

--- app.py
+++ app.py
@@ -24,15 +24,27 @@ def create_embedding(user_input):
     #embd = response.object.data[0].embedding
     return response.data[0].embedding
 
+def llm_analyze(query,user_profiles):
+   analysis = client.chat.completions.create(
+     model="gpt-4-turbo",
+     messages=[
+         {"role": "system", "content": """You are a helpful assistant of QNB team. QNB is a Turkish bank.
+         You will receive user profile summaries for 10 user at each time along with a search query QNB team wrote.
+         These users will be users that are retrieved regarding the query QNB team wrote. 
+         You need to post filter the search results are response the query by using user profile summaries you receive."""},
+         {"role": "user", "content": f""" Query: {query}/n/n User Profiles:{user_profiles}"""} ])
+   print(analysis)
+   return analysis
+
+
 # Function to query the Pinecone API
-def query_pinecone(vector):
+def query_pinecone(user_profiles):
     q_result = index.query(
         vector=vector,
         include_values=False,
         include_metadata=True,
         top_k=10,
     )
-
     return q_result
 
 # Streamlit app
@@ -49,9 +61,10 @@ if st.button("Submit"):
             # Query Pinecone
             pinecone_response = query_pinecone(embedding)
             summary_list = [i["metadata"]["summary"] for i in pinecone_response['matches']]
-
+            final_result = llm_analyze(user_input,summary_list)
             # Display the final answer
-            st.write("Pinecone Response:", summary_list)
+            st.write("LLM Analysis:", summary_list)
+            st.write("Search Results:", summary_list)
         except KeyError as e:
             st.error(f"KeyError: {e}. The expected key was not found in the response.")
         except Exception as e:

26085890ad16d590ff4326cdf15f779e9e3e3673
aktasbatuhan <101873025+aktasbatuhan@users.noreply.github.com>
2024-06-11 21:15:20 +0100

    changed vector_db

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 pc  = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
-index = pc.Index("qnbeyond")
+index = pc.Index("qnbeyond-2")
 
 client = OpenAI(
   api_key=os.environ['OPENAI_API_KEY'],
